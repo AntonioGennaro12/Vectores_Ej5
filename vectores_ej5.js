b90980b48f0b1fd97edd15eb03c7a8387428e571
Antonio Gennaro <antgennaro@hotmail.com>
2023-05-22 12:57:39 -0300

    Vectores Ej 5 - P3

--- vectores_ej5.js
+++ vectores_ej5.js
@@ -8,7 +8,6 @@ const AGREGA_MAX = 29;
 for(let cont=0; cont < AGREGA_MAX; cont++) {
     numeros.push((cont+2)*2);
 }
-
 console.log(numeros);
 
 // Parte 2: Se incrementa en "1" a todos los elementos del Array (vector)
@@ -17,5 +16,12 @@ const NUM_A_SUMAR   = 1;
 for (let cont=0; cont < numeros.length;cont++) {
     numeros[cont] += NUM_A_SUMAR;
 }
-
 console.log(numeros);
+
+// Parte 3: Se duplican todos los elementos del Array (vector)
+const NUM_A_MULTIP   = 2;
+for (let cont=0; cont < numeros.length;cont++) {
+    numeros[cont] *= NUM_A_MULTIP;
+}
+console.log(numeros);
+
